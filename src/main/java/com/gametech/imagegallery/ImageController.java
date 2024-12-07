178818ddec8b10c26fe0733787d8abade1e7afe6
nifmynif <115931873+nifmynif@users.noreply.github.com>
2024-12-07 12:20:06 +0300

    Добавил классы работы с изображениями

---
+++ src/main/java/com/gametech/imagegallery/ImageController.java
@@ -0,0 +1,55 @@
+package com.gametech.imagegallery;
+
+import javafx.scene.image.Image;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class ImageController {
+    public ImageHandler images;
+    private static ImageController imageController;
+
+    private ImageController() {
+        this.images = new ImageHandler();
+        initialize();
+    }
+
+    public static ImageController getImageController() {
+        if (imageController == null)
+            imageController = new ImageController();
+        return imageController;
+    }
+
+    public void initialize() {
+        File folder = new File("assets");
+        Arrays.stream(Objects.requireNonNull(folder.listFiles()))
+                .forEach(file -> {
+                    try {
+                        checkImage(file);
+                        images.getImages().put(file.getName(), new Image(file.toURI().toURL().toExternalForm()));
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public void getImage(String fileName) {
+        images.setCur(images.getImages().get(fileName));
+    }
+
+    private void checkImage(File file) throws IllegalArgumentException {
+        String fileName = file.getName();
+        if (!file.isFile() ||
+                !file.exists())
+            throw new IllegalArgumentException("Файл не существует или это не файл: " + fileName);
+
+        if (!fileName.endsWith(".jpg") &&
+                !fileName.endsWith(".jpeg") &&
+                !fileName.endsWith(".png"))
+            throw new IllegalArgumentException("Это не картинка: " + fileName);
+    }
+}
---
+++ src/main/java/com/gametech/imagegallery/ImageHandler.java
@@ -0,0 +1,43 @@
+package com.gametech.imagegallery;
+
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+
+
+public class ImageHandler {
+    private final HashMap<String, Image> images = new HashMap<>();
+
+    private Image prev;
+    private Image cur;
+
+    public HashMap<String, Image> getImages() {
+        return images;
+    }
+
+    public Image getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Image prev) {
+        this.prev = prev;
+    }
+
+    public Image getCur() {
+        return cur;
+    }
+
+    public void setCur(Image cur) {
+        this.cur = cur;
+    }
+
+    public Image getNext() {
+        return next;
+    }
+
+    public void setNext(Image next) {
+        this.next = next;
+    }
+
+    private Image next;
+}
